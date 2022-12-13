8aa3ab6d05e37c790345d723aab3c4384f5ab9d3
MrRamis <99258165+MrRamis@users.noreply.github.com>
2022-12-14 01:01:01 +0300

    C++ UI menu

---
+++ Source/No_d_3rdP/Private/Menu/STUMenuUserWidget.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Menu/STUMenuUserWidget.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
---
+++ Source/No_d_3rdP/Public/Menu/STUMenuUserWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "STUMenuUserWidget.generated.h"
+
+
+/**
+ * 
+ */
+UCLASS()
+class NO_D_3RDP_API USTUMenuUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
