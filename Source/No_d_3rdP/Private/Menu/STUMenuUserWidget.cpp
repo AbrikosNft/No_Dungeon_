d7eb047b3ceda504b56f9874fd2c55400a72638d
MrRamis <99258165+MrRamis@users.noreply.github.com>
2022-12-15 01:38:01 +0300

    C++ Menu

--- Source/No_d_3rdP/Private/Menu/STUMenuUserWidget.cpp
+++ Source/No_d_3rdP/Private/Menu/STUMenuUserWidget.cpp
@@ -4,4 +4,61 @@
 #include "Menu/STUMenuUserWidget.h"
 #include "Components/Button.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 
+void USTUMenuUserWidget::NativeOnInitialized()
+{
+    Super::NativeOnInitialized();
+
+    //if (SearchButton)
+    //{
+    //    SearchButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnSearch);
+    //}
+    //if (SoloButton)
+    //{
+    //    SoloButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnSolo);
+    //}
+    //if (ShopButton)
+    //{
+    //    ShopButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnShop);
+    //}
+    //if (DiaryButton)
+    //{
+    //    DiaryButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnDiary);
+    //}
+    //if (StatisticsButton)
+    //{
+    //    StatisticsButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnStatistics);
+    //}
+    //if (GalleryButton)
+    //{
+    //    GalleryButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnGallery);
+    //}
+    //if (SettingsButton)
+    //{
+    //    SettingsButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnSettings);
+    //}
+    if (ExitButton)
+    {
+        ExitButton->OnClicked.AddDynamic(this, &USTUMenuUserWidget::OnExit);
+    }
+}
+
+void USTUMenuUserWidget::OnSearch() {}
+
+void USTUMenuUserWidget::OnSolo() {}
+
+void USTUMenuUserWidget::OnShop() {}
+
+void USTUMenuUserWidget::OnDiary() {}
+
+void USTUMenuUserWidget::OnStatistics() {}
+
+void USTUMenuUserWidget::OnGallery() {}
+
+void USTUMenuUserWidget::OnSettings() {}
+
+void USTUMenuUserWidget::OnExit()
+{
+    UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
--- Source/No_d_3rdP/Public/Menu/STUMenuUserWidget.h
+++ Source/No_d_3rdP/Public/Menu/STUMenuUserWidget.h
@@ -3,10 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
 #include "Blueprint/UserWidget.h"
 #include "STUMenuUserWidget.generated.h"
 
-
+class UButon;
 /**
  * 
  */
@@ -15,4 +16,57 @@ class NO_D_3RDP_API USTUMenuUserWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UButton* ExitButton;
+    /*UPROPERTY(meta = (BindWidget))
+    UButton* SearchButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* SoloButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* ShopButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* DiaryButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* StatisticsButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* GalleryButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* SettingsButton;*/
+
+    
+
+	virtual void NativeOnInitialized() override;
+
+private:
+    UFUNCTION()
+    void OnSearch();
+
+	UFUNCTION()
+    void OnSolo();
+
+	UFUNCTION()
+    void OnShop();
+
+	UFUNCTION()
+    void OnDiary();
+
+	UFUNCTION()
+    void OnStatistics();
+
+	UFUNCTION()
+    void OnGallery();
+
+	UFUNCTION()
+    void OnSettings();
+
+	UFUNCTION()
+    void OnExit();
+
 };
